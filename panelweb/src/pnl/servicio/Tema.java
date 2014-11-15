cc2aca0a61609a3c24778513c6a0fb9af2f21206
manuelgarcia <manuelgarcia@192.168.0.45>
2014-11-15 07:52:01 -0500

    actualizar

---
+++ panelweb/src/pnl/servicio/Tema.java
@@ -0,0 +1,33 @@
+package pnl.servicio;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="tema", eager = true)
+@SessionScoped
+public class Tema implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String tema;
+	
+	@PostConstruct
+	public void init(){
+		tema="blitzer";
+	}
+
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+		
+
+}
